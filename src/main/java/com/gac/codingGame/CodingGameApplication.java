dcaa51fe851c1174e2fc2e467b4d16e5f96d5b63
Purbai <varsani_p@hotmail.com>
2025-09-10 11:07:15 +0100

    first commit

---
+++ src/main/java/com/gac/codingGame/CodingGameApplication.java
@@ -0,0 +1,13 @@
+package com.gac.codingGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingGameApplication.class, args);
+	}
+
+}
